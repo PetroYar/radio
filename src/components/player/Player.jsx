c740fd5182fde9c0dcfad2b65c64dbccdf96e59b
PetroYar <petryarish@gmail.com>
2024-10-15 17:02:51 +0300

    player

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -8,12 +8,15 @@ import iconPlay from "../icon/play.svg";
 import iconStop from "../icon/stop.svg";
 import random from "../icon/random.svg";
 import iconVolume from "../icon/volume.svg";
+import radioIcon from "../icon/radioIcon.png";
+
+
 const Player = (props) => {
   const audioRef = useRef();
   const {  radioStations, stationIndex, setStationIndex } =
     useRadio();
   const [currentTime, setCurrentTime] = useState(0);
-
+const [playbackStatus,setPlaybackStatus] = useState(false)
 
   const handleTimeUpdate = () => {
     setCurrentTime(audioRef.current.currentTime);
@@ -39,10 +42,12 @@ const Player = (props) => {
   };
   const play = () => {
     audioRef.current.play();
+    setPlaybackStatus(true);
     
   };
   const stop = () => {
     audioRef.current.pause();
+    setPlaybackStatus(false);
   };
   const playRandomStation = () => {
     const randomNum = Math.floor(Math.random() * radioStations.length);
@@ -68,13 +73,18 @@ const Player = (props) => {
             src={radioStations[stationIndex].url}
             ref={audioRef}
             onTimeUpdate={handleTimeUpdate}
-         
             className="player__audio"
           ></audio>
-          <button className="player__stop" onClick={stop}>
+          <button
+            className={`player__stop ${!playbackStatus ? "playing" : ""}`}
+            onClick={stop}
+          >
             <img src={iconStop} alt="stop" />
           </button>
-          <button onClick={play}>
+          <button
+            onClick={play}
+            className={`player__play ${playbackStatus ? "playing" : ""}`}
+          >
             <img src={iconPlay} alt="play" />
           </button>
           <button
@@ -85,11 +95,12 @@ const Player = (props) => {
             <img src={next} alt="next arrow" />
           </button>
 
-          <div className="player__nav "></div>
-          <div className="player__range-slider range-slider">
-            <span className=" range-slider__time">{formatTime(currentTime)}</span>
-           
-          </div>
+          
+          
+            <span className="time">
+              {formatTime(currentTime)}
+            </span>
+         
           <p className="player__station-name">
             {radioStations[stationIndex].name
               .replace(
@@ -108,9 +119,15 @@ const Player = (props) => {
             />
           </div>
 
-          {/* {radioStations[stationIndex].url ? (
-            <img className="test-img" src={radioStations[stationIndex].favicon} alt="" />
-          ): <p>nema</p>} */}
+          {radioStations[stationIndex].favicon ? (
+            <img
+              className="player__img"
+              src={radioStations[stationIndex].favicon}
+              alt="icon station"
+            />
+          ) : (
+            <img className="player__img" src={radioIcon} alt="radio" />
+          )}
         </>
       ) : (
         <p>Loading stations...</p> // Повідомлення, якщо станції ще не завантажилися
